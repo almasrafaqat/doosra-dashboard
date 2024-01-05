ac30a1a28f52f9d6228e5a83810b307861720ba7
Almas <almasrafaqat6@gmail.com>
2024-01-05 15:41:00 +0500

    removed icons files

--- src/components/MdDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { IoIosArrowBack } from "react-icons/io";
-const MdDashboard: React.FC = () => {
-  return (
-    <IoIosArrowBack />
-  );
-};
-
-export default MdDashboard;
