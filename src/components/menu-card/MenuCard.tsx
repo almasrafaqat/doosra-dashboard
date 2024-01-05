808ca7da514f054c92880be181393850af82a095
Almas <almasrafaqat6@gmail.com>
2024-01-05 16:06:17 +0500

    Navlink Removed

--- src/components/menu-card/MenuCard.tsx
+++ src/components/menu-card/MenuCard.tsx
@@ -1,5 +1,5 @@
 import  { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 interface MenuItem {
